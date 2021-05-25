3c8eb089c0a74ad70115f7d6e5d90bfe7024115a
Faubricio Chaves Hernández <faubricio98@gmail.com>
2021-05-25 00:16:24 -0600

    (CRS) Opción de descarga de la imagen

--- sunflower/js/script.js
+++ sunflower/js/script.js
@@ -1,10 +1,13 @@
 function clickSunFlower() {
     Swal.fire({
-        title: 'Sweet!',
-        text: 'Modal with a custom image.',
         imageUrl: 'img/agradecimientos/agradecimiento_4.svg',
         imageWidth: 500,
         imageHeight: 700,
         imageAlt: 'Custom image',
+        showConfirmButton: false,
+        html:
+            '<div>' +
+            '<a class="btn btn-primary" style="text-decoration: none; color: white;" href="img/agradecimientos/agradecimiento_4.svg" download>Descargar</a>'+
+            '</div>',
     })
 }
