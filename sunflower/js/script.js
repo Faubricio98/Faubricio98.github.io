1397a6dc233c9bd0d7cd0918db443686b045e0b8
Faubricio Chaves Hernández <faubricio98@gmail.com>
2021-05-27 17:26:26 -0600

    Cambio imágenes, tallo, y color fractal

--- sunflower/js/script.js
+++ sunflower/js/script.js
@@ -1,13 +1,14 @@
 function clickSunFlower() {
+    var aleatorio = Math.floor(Math.random() * (5 - 1)) + 1;
     Swal.fire({
-        imageUrl: 'img/agradecimientos/agradecimiento_4.svg',
+        imageUrl: 'img/agradecimientos/agradecimiento_'+aleatorio+'.svg',
         imageWidth: 500,
         imageHeight: 700,
         imageAlt: 'Custom image',
         showConfirmButton: false,
         html:
             '<div>' +
-            '<a class="btn btn-primary" style="text-decoration: none; color: white;" href="img/agradecimientos/agradecimiento_4.svg" download>Descargar</a>'+
+            '<a class="btn btn-primary" style="text-decoration: none; color: white;" href="img/agradecimientos/agradecimiento_'+aleatorio+'.svg" download>Descargar</a>'+
             '</div>',
     })
 }
